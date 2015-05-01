3cae84c35fcc2f01a6e0afb3b83be7f9d5e0ed98
John Stanhope <JohnStanhope@JohnStanhope.local>
2015-05-01 09:44:52 -0400

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,33 @@
 (function(){
   $(document).ready(function(){
+    $(".video").fitVids();
+
+    $(".modal-link").click(function() {
+    $("body").addClass("modal-open"),
+    $(".modal-bg").fadeIn(),
+    $(".modal-close").fadeIn(),
+    $(".modal-container").fadeIn()
+    });
+
+    $(".modal-close").click(function() {
+    $("body").removeClass("modal-open"),
+    $(".modal-bg").fadeOut(),
+    $(".modal-close").fadeOut(),
+    $(".modal-container").fadeOut()
+    });
+
     (function(){
       var active = false,
           $header = $('.header');
 
       $('[data-menu-toggle]').click(function(){
         if(active){
-          $header.removeClass('is-active');           
+          $header.removeClass('is-active');
           active = false;
         }else {
           $header.addClass('is-active');
           active = true;
-        } 
+        }
       });
     }());
   });
